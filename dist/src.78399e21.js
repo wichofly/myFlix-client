f1fda056c8772cfd59e81b9e57c6f95fac36c4e5
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-27 23:00:29 +0200

    Revert "installing parcel as devdependencies"
    This reverts commit 5d183974b42b0344f0cc4c798afd91494de48c7f.

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33237,7 +33237,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54239" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54041" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
