783a3b7d79bd182c75d3c4ffd6137009e09e48ed
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-28 22:42:10 +0200

    Problem 2
    To change a state, React wants you to use the this.setState method

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -30,7 +30,7 @@ export class MainView extends React.Component {
   
     return (
       <div className="main-view">
-        {movies.map(movie => <MovieCard key={movie._id} onMovieClick={(newSelectedMovie) => { this.state.selectedMovie = newSelectedMovie; }} />)}
+        {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setState({ selectedMovie: newSelectedMovie }); }} />)}
       </div>
     );
   }
