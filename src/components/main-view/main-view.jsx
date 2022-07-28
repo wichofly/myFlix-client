32c995cfde671207d464e13ea0da672620e21a5b
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-28 17:56:53 +0200

    selectedMovie
    This tells the application that no movie cards were clicked. But if a user clicked on a movie card, you’d need to update the selectedMovie state to refer to the movie object that was clicked, thus inducing the application to render that movie’s details.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -14,8 +14,9 @@ export class MainView extends React.Component {
         { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
         { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
         { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-      ]
-    }
+      ],
+      selectedMovie: null
+    };
   }
 
   // Condensed code
