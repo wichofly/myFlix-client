ab20309744c20d8c4841fc8b58283077a9b13a75
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-28 23:53:24 +0200

    Adding Genre and director

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27253,21 +27253,23 @@ class MainView extends (0, _reactDefault.default).Component {
             movies: [
                 {
                     _id: 1,
-                    Title: "Inception",
-                    Description: "desc1...",
-                    ImagePath: "..."
+                    Title: "Forest Gump",
+                    Description: "Forrest Gump, an innocent and kind-hearted Alabama boy, has been dealing with other peoples unkindness nearly all his life. Having grown up with beautiful Jenny, his only friend, Forrest yearns to learn all about the ways of the world and embarks on a mission to find his true purpose in life.",
+                    ImagePath: "https://www.imdb.com/title/tt0109830/mediaviewer/rm1954748672/?ref_=tt_ov_i"
                 },
                 {
                     _id: 2,
                     Title: "The Shawshank Redemption",
-                    Description: "desc2...",
-                    ImagePath: "..."
+                    Description: "It tells the story of banker, who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence.",
+                    ImagePath: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i"
                 },
                 {
                     _id: 3,
                     Title: "Gladiator",
-                    Description: "desc3...",
-                    ImagePath: "..."
+                    Description: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+                    Genre: "Action",
+                    Director: "Ridley Scott",
+                    ImagePath: "https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=tt_ov_i"
                 }
             ],
             selectedMovie: null
