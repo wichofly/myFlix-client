5762316b617fba3fd0fcdfed1f833fa6069ff3e4
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-01 10:58:34 +0200

    Importing Axios in MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'; // It will help to perform an ajax operation to call the movies from myFlix API
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -11,13 +12,21 @@ export class MainView extends React.Component {
   constructor(){ 
     super();      // initializes your component’s state, and without it, you’ll get an error if you try to use this.state inside constructor(). 
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Forest Gump', Description: 'Forrest Gump, an innocent and kind-hearted Alabama boy, has been dealing with other peoples unkindness nearly all his life. Having grown up with beautiful Jenny, his only friend, Forrest yearns to learn all about the ways of the world and embarks on a mission to find his true purpose in life.', Genre: 'Drama', Director: 'Robert Zemeckis', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'It tells the story of banker, who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence.', Genre: 'Drama', Director: 'Frank Darabont', ImagePath: 'https://www.kingwiki.de/images/6/6a/Die_Verurteilten%28Film%29.jpg'},
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', Genre:'Action', Director:"Ridley Scott", ImagePath: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png'}
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount(){
+    axios.get('https://wichoflix.herokuapp.com//movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -28,17 +37,17 @@ export class MainView extends React.Component {
 
   // Condensed code
   render() {
-    const { movies,selectedMovie } = this.state;
-  
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-  
+    const { movies, selectedMovie } = this.state;
+
+    if (movies.length === 0) return <div className="main-view" />;
+
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-          ))
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+         ))
         }
       </div>
     );
