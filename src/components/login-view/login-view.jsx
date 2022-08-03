d082a8566c661a53bab5ed878e4f15087a47106e
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-03 19:55:59 +0200

    delete changes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,13 +8,12 @@ import "./login-view.scss";
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  // change 2
+ 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */
     /* then call props.onLoggedIn(username) */
-    // change 3
     props.onLoggedIn(username);
   };
 
