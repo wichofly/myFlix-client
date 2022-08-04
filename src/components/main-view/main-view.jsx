a38d993e26f12987acde8a6514e194b67e1c9e35
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-04 13:38:47 +0200

    Update onLoggedIn

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -37,6 +37,21 @@ export class MainView extends React.Component {
       });
   }
 
+  getMovies(token) {
+    axios.get('https://wichoflix.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      // Assign the result to the state
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
   setSelectedMovie(movie) {
     this.setState({
@@ -45,10 +60,15 @@ export class MainView extends React.Component {
   }
 
   /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user,
+      user: authData.user.Username
     });
+  
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
   }
 
   // When an user succesfully register, this function updates the user properties
