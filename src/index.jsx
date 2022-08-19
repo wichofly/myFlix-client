a5cc9af256766ff7dcceea6e846d604434dad79a
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-19 13:02:02 +0200

    creating the store
    VS recommended me to use configureStore from '@reduxjs/toolkit'. i installed that npm too but i couldn't use it. it gave me an error in the browser.
    
    I installed devTools, I followed the example of the exercise, the problem I have is that I can't see redux when inspecting my browser.

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap'; // It is another way to import Bootstrap component
-// import ReactDOM from 'react-dom';
+import { Container } from 'react-bootstrap'; // It is another way to import Bootstrap component without "/container" but using "{}" at the begininng
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 // MainView is in enclosed in curly braces because was exported without the "default" keyword in "main-view.jsx"
 import { MainView } from './components/main-view/main-view';
-
 // Import statement to indicate that you need to bundle
 import './index.scss';
+import moviesApp from './reducers/reducers';
+
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
